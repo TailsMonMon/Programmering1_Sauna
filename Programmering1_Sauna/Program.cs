b08ca89e5bbafdec5e9382488e300b642e7e65b7
Monica Björk <m.bjork95@gmail.com>
2018-04-28 18:33:50 +0200

    Add project files.

---
+++ Programmering1_Sauna/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmering1_Sauna {
+    class Program {
+        static void Main(string[] args) {
+
+            //  DEFINING SOME DATA
+            int currentTemperature = 167;
+            double celsTemp = 75;
+            int fahrTemp = 167;
+            bool endLoop = false;
+
+            const int minTempF = 163;
+            const int maxTempF = 170;
+
+            double maxTempC = FahrToCels(maxTempF);
+            double minTempC = FahrToCels(minTempF);
+
+            //      INPUT
+            Console.WriteLine("=== SAUNA ===");
+            do {
+                do {
+                    Console.Write("Choose a temperature between {0} and {1}: ", minTempF, maxTempF);
+                    try {
+                        fahrTemp = Convert.ToInt32(Console.ReadLine());
+                        endLoop = true; /* To end the do-while loop when after the user 
+                                           has enterd an integer without any exceptions. */
+                    }
+                    catch(FormatException) { 
+                        Console.WriteLine("Please write an integer.");
+                    }
+                    catch(OverflowException) {
+                        Console.WriteLine("That number is too big or too small.");
+                    }
+                } while(endLoop != true);
+
+                celsTemp = FahrToCels(fahrTemp);    // To convert fahrenheit to celcius 
+                //Console.WriteLine("Control: {0:f1}Celsius",celsTemp);
+
+                //      OUTPUT
+                if(celsTemp < minTempC) { // To prevent the sauna from being too cold
+                    Console.WriteLine("That is too cold");
+                }
+                else if(celsTemp > maxTempC) { // To prevent the sauna from being too warm
+                    Console.WriteLine("That is too warm");
+                }
+                else {
+                    currentTemperature = fahrTemp;
+                    Console.WriteLine("The current temperature is now {0} fahrenheit", currentTemperature);
+                }
+
+            } while(celsTemp < minTempC || celsTemp > maxTempC);
+            Console.WriteLine("Thank you for this time");
+            Console.ReadKey();
+        }
+
+
+        /************** FahrToCels *************\
+        |  A method to convert a temperature    |
+        |  from fahrenheit to celsius           |
+        \***************************************/
+        static double FahrToCels(int fahrenheit) {
+            Convert.ToDouble(fahrenheit);   // To be able to calculate with decimals.
+            double celsius = (fahrenheit - 32) / 1.8;
+            return celsius;
+        }
+    }
+}
