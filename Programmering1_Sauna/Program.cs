c3c94762930e0bc6c49d7a0995fd60093bf360f4
Monica Björk <m.bjork95@gmail.com>
2018-04-29 15:21:07 +0200

    Added a new method that randomises a temperature.

--- Programmering1_Sauna/Program.cs
+++ Programmering1_Sauna/Program.cs
@@ -6,18 +6,22 @@ using System.Threading.Tasks;
 
 namespace Programmering1_Sauna {
     class Program {
+
+        //  DEFINING SOME DATA
+        const int minTempF = 163;
+        const int maxTempF = 170;
+
+        static double maxTempC = FahrToCels(maxTempF);
+        static double minTempC = FahrToCels(minTempF);
+        // This data can be used in all methods.
+
+
         static void Main(string[] args) {
 
-            //  DEFINING SOME DATA
+            //  DEFINING SOME MORE DATA
             int fahrTemp = 167;
             double celsTemp = 75;
             bool endLoop = false;
-
-            const int minTempF = 163;
-            const int maxTempF = 170;
-
-            double maxTempC = FahrToCels(maxTempF);
-            double minTempC = FahrToCels(minTempF);
 
             //      INPUT
             Console.WriteLine("=== SAUNA ===");
@@ -37,34 +41,51 @@ namespace Programmering1_Sauna {
                     }
                 } while(endLoop != true);
 
-                celsTemp = FahrToCels(fahrTemp);    // To convert fahrenheit to celcius 
-                //Console.WriteLine("Control: {0:f1}Celsius",celsTemp);
+                if(fahrTemp != 0) {
+                    celsTemp = FahrToCels(fahrTemp);    // To convert fahrenheit to celcius 
+                    //Console.WriteLine("Control: {0:f1} Celsius",celsTemp);
 
-                //      OUTPUT
-                if(celsTemp < minTempC) { // To prevent the sauna from being too cold
-                    Console.WriteLine("That is too cold");
-                }
-                else if(celsTemp > maxTempC) { // To prevent the sauna from being too warm
-                    Console.WriteLine("That is too warm");
+                    //      OUTPUT
+                    if(celsTemp < minTempC) { // To prevent the sauna from being too cold
+                        Console.WriteLine("That is too cold");
+                    }
+                    else if(celsTemp > maxTempC) { // To prevent the sauna from being too warm
+                        Console.WriteLine("That is too warm");
+                    }
+                    else {
+                        Console.WriteLine("The current temperature is now {0} fahrenheit", fahrTemp);
+                    }
                 }
                 else {
-                    Console.WriteLine("The current temperature is now {0} fahrenheit", fahrTemp);
+                    FahrToCels();
                 }
-
             } while(celsTemp < minTempC || celsTemp > maxTempC);
             Console.WriteLine("Thank you for this time");
             Console.ReadKey();
         }
 
 
-        /************** FahrToCels *************\
-        |  A method to convert a temperature    |
-        |  from fahrenheit to celsius           |
-        \***************************************/
+        /************* FahrToCels *************\
+        |  A method to convert a temperature   |
+        |  from fahrenheit to celsius          |
+        \**************************************/
         static double FahrToCels(int fahrenheit) {
             Convert.ToDouble(fahrenheit);   // To be able to calculate with decimals.
             double celsius = (fahrenheit - 32) / 1.8;
             return celsius;
+        }
+
+        /************* FahrToCels *************\
+        |  A method that choosed a random      |
+        |  temperature between min and max     |
+        \**************************************/
+        static void FahrToCels() {
+            Random rnd = new Random();
+            int rndTempF = rnd.Next(minTempF, maxTempF);
+            Console.WriteLine("The current temperature is now {0} fahrenheit", rndTempF);
+            double celsius = (rndTempF - 32) / 1.8;
+            //Console.WriteLine("Control: {0:f1} Celsius", celsius);
+            return;
         }
     }
 }
